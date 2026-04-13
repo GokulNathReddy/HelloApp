5580a289944d14fca57b7ad8016155f6d68b3974
GokulReddy <142143957+GokulNathReddy@users.noreply.github.com>
2026-04-13 08:29:59 +0530

    updated

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -2,10 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (String name : args) {
+                if (sb.length() > 0) {
+                    sb.append(", ");
+                }
+                sb.append(name);
+            }
+            System.out.println("Hello, " + sb + "!");
         }
     }
 }
